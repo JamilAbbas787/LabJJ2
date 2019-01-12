fe87a61917f5b04dc8fbb0e0fb36481b7cdfa5b8
Jamil Abbas <jamilabbas787@gmail.com>
2019-01-12 17:26:23 -0500

    Mostly Complete

---
+++ LabJJ2/LabJJ2/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LabJJ2.Models;
+
+namespace LabJJ2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index(Products model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToRoute(new { controller = "Home", action = "Index" });
+            }
+
+            return View("Index", model);
+        }
+
+        //public ActionResult Add(Products model)
+        //{
+        //    return View("Index", model);
+        //}
+
+    }
+}
---
+++ LabJJ2/LabJJ2/Models/Products.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabJJ2.Models
+{
+    public class Products
+    {
+
+        // Name(Required, Maxlength 20)
+        [DisplayName("Product Name")]
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+
+        //Description(Required, Maxlength 50)
+        [DisplayName("Description")]
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+
+        // Price(Required, Decimal)
+        [DisplayName("Price")]
+        [Required]
+        public decimal Price { get; set; }
+
+    }
+}
